6201c29df8212aa5d79c78414154919c8d8b03e4
Marcian Diamond <marciandmnd@yahoo.com>
2017-12-21 12:46:41 -0800

    Body parsing and CORS handling

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -7,10 +7,16 @@ router.get('/', (req, res, next) => {
   });
 });
 
-router.post('/:orderId', (req, res, next) => {
+router.post('/', (req, res, next) => {
+  const order = {
+    productId: req.body.productId,
+    quantity: req.body.quantity
+  };
+
   res.status(201).json({
-    message: 'Order was created!'
-  })
+    message: 'Order was created!',
+    order: order
+  });
 });
 
 router.get('/:orderId', (req, res, next) => {
